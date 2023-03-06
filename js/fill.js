dda0595e849eda72fa57350b34d24a2dfbcd6f1b
Neider <neidergainza1@gmail.com>
2023-03-06 01:19:53 +0100

    Arreglos finales.

--- js/fill.js
+++ js/fill.js
@@ -1,56 +1,54 @@
 const fill = {
-    "name": "Neider Gainza Llacer",
-    "ocupation": {"es":"Desarrollador de Software", "en":"Jr Sofware Developend"},
+    "name": "Neider Gainza Llacer.",
+    "ocupation": {"es":"Desarrollador de Software.", "en":"Jr Sofware Developer."},
     "about_me": {"es":"SOBRE MI", "en":"ABOUT ME"},
-    "description": {"es":" no aplica ", "en":" not able texto de prueba, me llamo neider gainza llacer y quiero que me pagues muuuucho pero es complicado porque en realidad no se muchas cosas pero igual vas a pagarme no o si o no "},
+    "description": {"es":" Estudiante de Ciencias de la Computación, con resultdos relevantes en olimpiadas nacionales y regionales de matemáticas y programación competitiva, en busca de comenzar su vida laboral como desarrollador de software.", "en":"Computer Science student, with relevant results in national and regional math and competitive programming olympics, looking to start his working life as a software developer."},
     // "contacts": {"es":"CONTACTOS", "en":"CONTACTS"},
     "phone_number": "+5355306873",
     "email": "neidergainza1@gmail.com",
     "location": {"es":"Guantánamo, Cuba", "en":"Guantanamo, Cuba"},
     "education":[
-        {"es":"EDUCACIÓN", "en":"EDUCATION"},
-        {"en":"BACHELOR'S DEGREE", "es":"BACHILLERATO"},
-        "IPVCE José Maceo Grajales",
-        "2016 - 2019",
-        {"en":"College Degree in Computer Science", "es":"Licenciatura en Ciencias de la Computación"},
-        {"es":"Universidad de Oriente, Cuba", "en":"Eastern University, Cuba"},
-        {"es":"2020 - aún", "en":"2020 - still"},
+        {"es":"EDUCACIÓN:", "en":"EDUCATION:"},
+        {"en":"BACHELOR'S DEGREE.", "es":"BACHILLERATO."},
+        "IPVCE José Maceo Grajales.",
+        "2016 - 2019.",
+        {"en":"College Degree in Computer Science.", "es":"Licenciatura en Ciencias de la Computación."},
+        {"es":"Universidad de Oriente, Cuba.", "en":"Eastern University, Cuba."},
+        {"es":"2020 - aún.", "en":"2020 - still."},
     ],
     "skills":[
-        {"es":"HABILIDADES", "en":"SKILLS"},
-        "C, C++",
-        "Python, Django",
-        "Git, Github",
-        {"es":"SQL(nivel básico)", "en":"SQL(basic level)"},
-        {"es":"HTML,CSS,JS(nivel básico)", "en":"HTML,CSS,JS(basic level)"},
-        {"en":"Good at math", "es":"Bueno en matemáticas"},
+        {"es":"HABILIDADES:", "en":"SKILLS:"},
+        "C, C++.",
+        "Python, Django.",
+        "Git, Github.",
+        {"es":"SQL(nivel básico).", "en":"SQL(basic level)."},
+        {"es":"HTML,CSS,JS(nivel básico).", "en":"HTML,CSS,JS(basic level)."},
+        {"en":"Good at math.", "es":"Bueno en matemáticas."},
     ],
     "achievements": [
-        {"en":"ACHIEVEMENTS", "es":"LOGROS"},
-        {"es":"Clasificación a la Final Caribeña de ICPC (2022 y 2023)", "en":"Cualification to the ICPC Caribeean Final (2022 and 2023)"},
-        {"es":"Dos veces preselección nacional para equipo Cuba IMO", "en":"Twice naciotal preselection for Team Cuba IMO"},
-        {"es":"Tres veces medallista en la olimpiada nacional preuniversitaria de matemática cubana( plata en 2019 y bronce en los años 2017 y 2018)", "en":"Three times medalist in the cuban national pre-university mathematical olympiad (silver in 2019 and bronze in 2017 and 2018)"},
-        {"es": "Participación en el campeonato nacional juenil de ajedrez cubano en el año 2017", "en":"Participation in the Cuban National Youth Chess Championship in 2017"},
+        {"en":"ACHIEVEMENTS:", "es":"LOGROS:"},
+        {"es":"Clasificación a la Final Caribeña de ICPC (2022 y 2023).", "en":"Cualification to the ICPC Caribeean Final (2022 and 2023)."},
+        {"es":"Dos veces preselección nacional para equipo Cuba IMO.", "en":"Twice naciotal preselection for Team Cuba IMO."},
+        {"es":"Tres veces medallista en la olimpiada nacional preuniversitaria de matemática cubana( plata en 2019 y bronce en los años 2017 y 2018).", "en":"Three times medalist in the cuban national pre-university mathematical olympiad (silver in 2019 and bronze in 2017 and 2018)."},
+        {"es": "Participación en el campeonato nacional juenil de ajedrez cubano en el año 2017.", "en":"Participation in the Cuban National Youth Chess Championship in 2017."},
 
     ],
     "experience":[
-        {"es":"EXPERIENCIA", "en": "EXPERIENCE"},
-        {"es":"Proyectos como trabajador independiente disponibles en mi cuenta de GitHub", "en":"Projects as a freelacer abailable on my GitHub account"},
+        {"es":"EXPERIENCIA:", "en": "EXPERIENCE:"},
+        // {"es":"Proyectos como trabajador independiente disponibles en mi cuenta de GitHub", "en":"Projects as a freelacer abailable on my GitHub account"},
         {"es":"Experiencia como programador competitivo (C++ y Python)", "en":"Expirience as a competitive programmer (C++ and Python)"},
     ],
     "others":[
-        {"es":"OTRAS", "en":"OTHERS"},
-        {"es":"Español (nativo)", "en":"Espanish (native)"},
-        {"es":"Inglés (C1)", "en":"English (C1)"},
-        {"es":"Trabajo en equipo", "en": "Team Work"},
-        {"es":"Rápido aprendizaje", "en":"Fast Learning"},
+        {"es":"OTRAS:", "en":"OTHERS:"},
+        {"es":"Español (nativo).", "en":"Espanish (native)."},
+        {"es":"Inglés (B1)", "en":"English (B1)"},
+        {"es":"Trabajo en equipo.", "en": "Team Work."},
+        {"es":"Rápido aprendizaje.", "en":"Fast Learning."},
     ],
     "objetives":[
-        {"es":"OTRAS", "en":"OTHERS"},
-        {"es":"Español (nativo)", "en":"Espanish (native)"},
-        {"es":"Inglés (C1)", "en":"English (C1)"},
-        {"es":"Trabajo en equipo", "en": "Team Work"},
-        {"es":"Rápido aprendizaje", "en":"Fast Learning"},
+        {"es":"OBJETIVOS:", "en":"OBJETIVES:"},
+        {"es":"Acumular experiencia como desarrollador de software.", "en":"Accumulate experience as a software developer."},
+        {"es":"Concluir estudios.", "en":"Finish studies."},
     ],
 };
 
@@ -135,7 +133,7 @@ imagen.addEventListener("click",accion)
 imagen_perfil.addEventListener("click", accion)
 canvas.addEventListener("click", accion)
 
-
+console.log("asdasd")
 
 
 

fb2f619216eac0d810f792dfe5d297354ef73299
neiderGainza <111709054+neiderGainza@users.noreply.github.com>
2024-03-15 18:32:21 -0400

    Update fill.js
    Jsjsjdj

--- js/fill.js
+++ js/fill.js
@@ -35,7 +35,7 @@ const fill = {
     ],
     "experience":[
         {"es":"EXPERIENCIA:", "en": "EXPERIENCE:"},
-        {"Odoo freenlace developer for 6 months"},
+        {"en":"Odoo freenlace developer for 6 months"},
         // {"es":"Proyectos como trabajador independiente disponibles en mi cuenta de GitHub", "en":"Projects as a freelacer abailable on my GitHub account"},
         {"es":"Experiencia como programador competitivo (C++ y Python)", "en":"Expirience as a competitive programmer (C++ and Python)"},
     ],
